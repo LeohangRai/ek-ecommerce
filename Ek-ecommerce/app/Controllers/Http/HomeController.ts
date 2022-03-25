0ee03b10d2d598227be02ea46cf8c1e7f25c858b
LeohangRai <58897435+LeohangRai@users.noreply.github.com>
2022-03-25 16:14:25 +0545

    Views setup done. Default layout and partials created. The home page and error 404 pages have been created.

--- Ek-ecommerce/app/Controllers/Http/HomeController.ts
+++ Ek-ecommerce/app/Controllers/Http/HomeController.ts
@@ -1,3 +1,10 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class HomeController {}
+export default class HomeController {
+  public home({ view }: HttpContextContract) {
+    return view.render('home/index', { title: 'Ek-ecommerce Home' })
+  }
+  public error404({ view }: HttpContextContract) {
+    return view.render('404', { title: 'Error 404 not found' })
+  }
+}
