364df4d5c17d665ffea4f6c45c9ccdb1a5aaad37
LeohangRai <58897435+LeohangRai@users.noreply.github.com>
2022-03-30 14:14:18 +0545

    Database design updated. Favorites, Roles and permissions tables added to the design

--- Database Design/E-commerce project.sql
+++ Database Design/E-commerce project.sql
@@ -9,83 +9,78 @@ CREATE TABLE `customers` (
   `updated_at` timestamp
 );
 
-CREATE TABLE `product_variants` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `prodcut_id` int,
-  `stock_id` int,
-  `name` varchar(255),
-  `img` varchar(255)
+CREATE TABLE `favorites` (
+  `customer_id` int,
+  `product_id` int
 );
 
 CREATE TABLE `products` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `category_id` int,
-  `vendor_id` int,
   `brand_id` int,
   `name` varchar(255),
   `slug` varchar(255),
   `description` text,
   `price` double,
+  `discount_price` double,
   `delivery` boolean,
-  `created_at` timestamp
+  `image` varchar(255),
+  `status` boolean,
+  `created_at` timestamp,
+  `updated_at` timestamp
 );
 
 CREATE TABLE `categories` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255),
+  `slug` varchar(255),
+  `status` boolean,
   `created_at` timestamp,
-  `updated_at` timestamp,
-  `admin_id` int
-);
-
-CREATE TABLE `vendors` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `username` varchar(255),
-  `password` varchar(255),
-  `addresss` varchar(255),
-  `phone` int(10),
-  `products_count` int,
-  `status` boolean
+  `updated_at` timestamp
 );
 
 CREATE TABLE `brands` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255),
-  `products_count` int,
-  `admin_id` int
+  `products_count` int
 );
 
-CREATE TABLE `stocks` (
+CREATE TABLE `orders` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `total_stock` int,
-  `unit_price` double,
+  `customer_id` int NOT NULL,
+  `payment_id` int,
+  `quantity` int,
+  `order_number` int,
+  `status` boolean,
+  `totalPrice` double,
+  `created_at` timestamp
+);
+
+CREATE TABLE `order_items` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `order_id` int,
+  `product_id` int,
+  `quantity` int
+);
+
+CREATE TABLE `carts` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `customer_id` int,
   `total_price` double,
   `created_at` timestamp,
   `updated_at` timestamp
 );
 
-CREATE TABLE `orders` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `product_variant_id` int,
-  `customer_id` int NOT NULL,
-  `payment_id` int,
-  `quantity` int,
-  `totalPrice` double,
-  `created_at` timestamp
-);
-
-CREATE TABLE `carts` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `customer_id` int
-);
-
 CREATE TABLE `cart_products` (
   `cart_id` int,
-  `product_variant_id` int
+  `product_id` int,
+  `product_name` varchar(255),
+  `price` double
 );
 
 CREATE TABLE `payments` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
+  `transaction_id` int,
   `payment_type` enum,
   `payment_gateway` enum,
   `total` double,
@@ -93,8 +88,28 @@ CREATE TABLE `payments` (
   `created_at` timestamp
 );
 
+CREATE TABLE `roles` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `slug` varchar(255),
+  `description` text
+);
+
+CREATE TABLE `permissions` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `slug` varchar(255),
+  `description` text
+);
+
+CREATE TABLE `role_permission` (
+  `role_id` int,
+  `permission_id` int
+);
+
 CREATE TABLE `admins` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
+  `role_id` int,
   `username` varchar(255),
   `password` varchar(255),
   `status` boolean,
@@ -102,28 +117,42 @@ CREATE TABLE `admins` (
   `updated_at` timestamp
 );
 
-ALTER TABLE `product_variants` ADD FOREIGN KEY (`prodcut_id`) REFERENCES `products` (`id`);
+CREATE TABLE `cms_users` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `role_id` int,
+  `username` varchar(255),
+  `password` varchar(255),
+  `status` boolean,
+  `created_at` timestamp,
+  `updated_at` timestamp
+);
 
-ALTER TABLE `product_variants` ADD FOREIGN KEY (`stock_id`) REFERENCES `stocks` (`id`);
+ALTER TABLE `favorites` ADD FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`);
+
+ALTER TABLE `favorites` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
 
 ALTER TABLE `products` ADD FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
 
-ALTER TABLE `products` ADD FOREIGN KEY (`vendor_id`) REFERENCES `vendors` (`id`);
-
 ALTER TABLE `products` ADD FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`);
-
-ALTER TABLE `categories` ADD FOREIGN KEY (`admin_id`) REFERENCES `admins` (`id`);
-
-ALTER TABLE `brands` ADD FOREIGN KEY (`admin_id`) REFERENCES `admins` (`id`);
-
-ALTER TABLE `orders` ADD FOREIGN KEY (`product_variant_id`) REFERENCES `product_variants` (`id`);
 
 ALTER TABLE `orders` ADD FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`);
 
 ALTER TABLE `orders` ADD FOREIGN KEY (`payment_id`) REFERENCES `payments` (`id`);
 
+ALTER TABLE `order_items` ADD FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`);
+
+ALTER TABLE `order_items` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
 ALTER TABLE `carts` ADD FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`);
 
 ALTER TABLE `cart_products` ADD FOREIGN KEY (`cart_id`) REFERENCES `carts` (`id`);
 
-ALTER TABLE `cart_products` ADD FOREIGN KEY (`product_variant_id`) REFERENCES `product_variants` (`id`);
+ALTER TABLE `cart_products` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+ALTER TABLE `role_permission` ADD FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
+
+ALTER TABLE `role_permission` ADD FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`);
+
+ALTER TABLE `admins` ADD FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
+
+ALTER TABLE `cms_users` ADD FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
